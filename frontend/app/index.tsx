aa74bf7d19c31f41ae2add56bac15e60ee5a9619
Ethan Garcia <ethan2917@gmail.com>
2025-04-09 03:07:43 -0400

    Revert "Add settings page"
    This reverts commit cf01b292530bcd49f78edb5f1eb256db706a53be.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -235,11 +235,7 @@ export default function Index() {
           <Text style={styles.alertTitle}>Severe Weather Alerts</Text>
           <Text style={styles.alertSubtitle}>Wind Advisory, Troy, NY</Text>
           <View style={styles.weatherAlertsButton}>
-            <Button
-              title="Weather Alerts"
-              onPress={() => router.push("/settings")}
-              color="#fff"
-            />
+            <Button title="Weather Alerts" onPress={() => {}} color="#fff" />
           </View>
         </View>
       </View>
